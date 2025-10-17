4e0e56a89425978e71f88f4e81c2ae8ea39ed904
quemsi <quemsi.app@gmail.com>
2025-10-17 23:48:49 +0300

    azure blob storage fixes

--- src/main/java/com/quemsi/agent/AgentApplication.java
+++ src/main/java/com/quemsi/agent/AgentApplication.java
@@ -7,7 +7,12 @@ import org.springframework.context.annotation.ImportRuntimeHints;
 @SpringBootApplication(exclude = {
     org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
     org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
-    org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration.class
+    org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.http.codec.CodecsAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.web.reactive.ReactiveMultipartAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.web.reactive.ReactiveWebServerFactoryAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration.class
 })
 @ImportRuntimeHints(AgentRuntimeHintsRegistrar.class)
 public class AgentApplication {
